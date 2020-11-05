ba79a5174d4844a8787cc3df75b87bbfc10fc4e0
Locckhl <locckhl123@gmail.com>
2020-11-05 22:04:29 +0700

    added seeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'first_name' => Str::random(10),
+            'last_name' => Str::random(10),
+            'user_name' => 'test1',
+            'password' => Hash::make('123456'),
+            'gender' => 'male',
+            'birthday' => date('Y-m-d H:i:s',mt_rand(1, 2147385600)),
+            'email' => Str::random(10).'@gmail.com',
+            'phone' => '0123456789',
+            'address' => Str::random(20),
+            'job' => Str::random(10),
+            'avatar_url' => asset('personal-user-illustration-@2x.png'),
+        ]);
+        DB::table('users')->insert([
+            'first_name' => Str::random(10),
+            'last_name' => Str::random(10),
+            'user_name' => 'test2',
+            'password' => Hash::make('123456'),
+            'gender' => 'female',
+            'birthday' => date('Y-m-d H:i:s',mt_rand(1, 2147385600)),
+            'email' => Str::random(10).'@gmail.com',
+            'phone' => '0123456789',
+            'address' => Str::random(20),
+            'job' => Str::random(10),
+            'avatar_url' => asset('personal-user-illustration-@2x.png'),
+        ]);
+    }
+}
