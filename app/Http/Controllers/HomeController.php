57ce5f2d321168d0f61ad4a9cddee59dea370175
lequang-bk <55751386+lequang-hp@users.noreply.github.com>
2020-12-09 02:40:11 +0700

    Share tags variable to all view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,13 +9,13 @@ use App\Post;
 use phpDocumentor\Reflection\Types\Compound;
 use Session;
 use Illuminate\Support\Facades\Redirect;
+
 session_start();
 
 class HomeController extends Controller
 {
     public function index(){
-        $tags = DB::table("tags")->get();
         $posts = DB::table('posts')->orderBy('post_id','desc')->limit(3)->get();
-        return view('user.home',compact('posts'), compact('tags'));
+        return view('user.home',compact('posts'));
     }
 }
