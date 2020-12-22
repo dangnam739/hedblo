30baacc585cabe4f899aaf95b8e95b61c04c639a
dangnam739 <kieudangnam99@gmail.com>
2020-12-22 22:59:26 +0700

    fix small conflict

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -25,81 +25,92 @@ class PostController extends Controller
     }
 
     # Get all post
-    function all_post(Request $request){
+    function all_post(Request $request)
+    {
         $posts = Post::paginate(3);
         $title = "Posts";
-        if($request->ajax()){
-            return view('post.post_data', compact('posts','title'))->render();
+        if ($request->ajax()) {
+            return view('post.post_data', compact('posts', 'title'))->render();
         }
         return view('post.posts', compact('posts', 'title'));
     }
 
     # Get post by id
-    public function post_detail($post_id){
+    public function post_detail($post_id)
+    {
         $post = Post::find($post_id);
-        $recent_posts = Post::where('post_id','!=',$post_id)->orderBy('date_create','desc')->take(3)->get();
+        $recent_posts = Post::where('post_id', '!=', $post_id)->orderBy('date_create', 'desc')->take(3)->get();
 
         $comment_count = DB::table('comments')
-                            ->join('posts','comments.post_id','=','posts.post_id')
-                            ->where('posts.post_id','=',$post_id)
-                            ->count();
+            ->join('posts', 'comments.post_id', '=', 'posts.post_id')
+            ->where('posts.post_id', '=', $post_id)
+            ->count();
 
         $comments = DB::table('comments')
-                    ->join('posts','comments.post_id','=','posts.post_id')
-                    ->join('users','comments.user_id','=','users.user_id')
-                    ->where('posts.post_id','=',$post_id)
-                    ->select('comments.content','users.user_name','users.avatar_url')
-                    ->paginate(5);
+            ->join('posts', 'comments.post_id', '=', 'posts.post_id')
+            ->join('users', 'comments.user_id', '=', 'users.user_id')
+            ->where('posts.post_id', '=', $post_id)
+            ->select('comments.content', 'users.user_name', 'users.avatar_url')
+            ->paginate(5);
 
 
         $current_user = User::find(auth()->user()->user_id);
-        $search_user_post = DB::table('user_post_like')->where('user_id',$current_user->user_id)->where('post_id',$post_id)->first();
+        $search_user_post = DB::table('user_post_like')->where('user_id', $current_user->user_id)->where('post_id', $post_id)->first();
 
-        $count_like = DB::table('user_post_like')->where('post_id',$post_id)->where('like_state',1)->count();
+        $count_like = DB::table('user_post_like')->where('post_id', $post_id)->where('like_state', 1)->count();
 
-        if(!$search_user_post){
+        if (!$search_user_post) {
             $data = array();
             $data["user_id"] = $current_user->user_id;
             $data["post_id"] = $post_id;
             $data["like_state"] = 0;
             DB::table("user_post_like")->insert($data);
-            $search_user_post = DB::table('user_post_like')->where('user_id',$current_user->user_id)->where('post_id',$post_id)->select('like_state')->first();
-        }else{
-            $search_user_post = DB::table('user_post_like')->where('user_id',$current_user->user_id)->where('post_id',$post_id)->select('like_state')->first();
+            $search_user_post = DB::table('user_post_like')->where('user_id', $current_user->user_id)->where('post_id', $post_id)->select('like_state')->first();
+        } else {
+            $search_user_post = DB::table('user_post_like')->where('user_id', $current_user->user_id)->where('post_id', $post_id)->select('like_state')->first();
         }
 
-        $post_tags = Tag::whereHas('posts', function($query) use ($post_id) {
+        $post_tags = Tag::whereHas('posts', function ($query) use ($post_id) {
             $query->where('posts.post_id', $post_id);
         })->get();
 
-        return view('post.post_detail',compact('post','recent_posts',
-                        'comment_count','comments','current_user',
-                        'search_user_post','count_like','post_tags'));
+        return view('post.post_detail', compact(
+            'post',
+            'recent_posts',
+            'comment_count',
+            'comments',
+            'current_user',
+            'search_user_post',
+            'count_like',
+            'post_tags'
+        ));
     }
 
     # Get post by tag_id
-    public function post_tag(Request $request, $tag_id){
-        $posts = Post::whereHas('tags', function($query) use ($tag_id) {
+    public function post_tag(Request $request, $tag_id)
+    {
+        $posts = Post::whereHas('tags', function ($query) use ($tag_id) {
             $query->where('tags.tag_id', $tag_id);
         })->paginate(3);
 
         $tag = Tag::find($tag_id);
         $title = strtoupper($tag->tag_title);
 
-        if($request->ajax()){
-            return view('post.post_data', compact('posts','title'))->render();
+        if ($request->ajax()) {
+            return view('post.post_data', compact('posts', 'title'))->render();
         }
-        return view('post.posts',compact('posts','title'));
+        return view('post.posts', compact('posts', 'title'));
     }
 
     # Create new post
-    public function create(Request $request){
+    public function create(Request $request)
+    {
         $current_user = User::find(auth()->user()->user_id);
-        if($request->isMethod('post')){
+        if ($request->isMethod('post')) {
             # Validate post form data
             $request->validate(
                 [
-                    'title' => ['required','min:4','max:255'],
+                    'title' => ['required', 'min:4', 'max:255'],
                     'tags' => 'required',
                     'description' => 'required',
                     'detail_content' => 'required'
@@ -130,21 +141,22 @@ class PostController extends Controller
             foreach ($tags as $tag_id) {
                 $post->tags()->attach($tag_id);
             }
-            return redirect('/posts/'.$post_id);
+            return redirect('/posts/' . $post_id);
         }
         return view('post.create_post');
     }
 
     # Edit post
-    public function edit(Request $request,$post_id){
+    public function edit(Request $request, $post_id)
+    {
         $post = Post::find($post_id);
 
         $selected_tags_array = array();
-        foreach ($post->tags as $selected_tag){
+        foreach ($post->tags as $selected_tag) {
             array_push($selected_tags_array, $selected_tag->tag_id);
         }
 
-        if($request->isMethod('post')){
+        if ($request->isMethod('post')) {
             $post = Post::find($post_id);
             $post->title = $request->title;
 
@@ -161,24 +173,25 @@ class PostController extends Controller
             $post->description = $request->description;
             $post->date_create = date('Y-m-d');
 
-            $post_tag_list = PostTag::where('post_id',$post_id);
+            $post_tag_list = PostTag::where('post_id', $post_id);
             $post_tag_list->delete();
             $tags = $request->tags;
-            foreach($tags as $tag_id){
-            //    $post_tag = new PostTag();
-            //    $post_tag->post_id = $post_id;
-            //    $post_tag->tag_id = $tag_id;
-            //    $post_tag->save();
+            foreach ($tags as $tag_id) {
+                //                $post_tag = new PostTag();
+                //                $post_tag->post_id = $post_id;
+                //                $post_tag->tag_id = $tag_id;
+                //                $post_tag->save();
                 $post->tags()->attach($tag_id);
             }
             $post->save();
-            return redirect('/posts/'.$post_id);
+            return redirect('/posts/' . $post_id);
         }
-        return view('post.edit_post',compact('post','selected_tags_array'));
+        return view('post.edit_post', compact('post', 'selected_tags_array'));
     }
 
     # Delete post
-    public function delete($post_id){
+    public function delete($post_id)
+    {
         $post = Post::find($post_id);
         $post->delete();
         return redirect('/posts');
@@ -199,34 +212,35 @@ class PostController extends Controller
     }
 
     # Like action
-    public function react(Request $request){
+    public function react(Request $request)
+    {
         $post_id = $request->post_id;
         $user_id = (User::find(auth()->user()->user_id))->user_id;
-        $current_state = DB::table('user_post_like')->where('user_id',$user_id)->where('post_id',$post_id)->select('like_state')->first();
-        if($current_state->like_state == 0){
-            DB::table('user_post_like')->where('post_id',$post_id)->where('user_id',$user_id)->update(['like_state'=>1]);
-        }
-        else{
-            DB::table('user_post_like')->where('post_id',$post_id)->where('user_id',$user_id)->update(['like_state'=>0]);
+        $current_state = DB::table('user_post_like')->where('user_id', $user_id)->where('post_id', $post_id)->select('like_state')->first();
+        if ($current_state->like_state == 0) {
+            DB::table('user_post_like')->where('post_id', $post_id)->where('user_id', $user_id)->update(['like_state' => 1]);
+        } else {
+            DB::table('user_post_like')->where('post_id', $post_id)->where('user_id', $user_id)->update(['like_state' => 0]);
         }
 
-        $count_like = DB::table('user_post_like')->where('post_id',$post_id)->where('like_state',1)->count();
+        $count_like = DB::table('user_post_like')->where('post_id', $post_id)->where('like_state', 1)->count();
 
-        if($request->ajax()){
+        if ($request->ajax()) {
             return $count_like;
         }
-        return redirect('/posts/'.$post_id);
+        return redirect('/posts/' . $post_id);
     }
 
     # Get my posts
-    public function get_my_posts(Request $request){
+    public function get_my_posts(Request $request)
+    {
         $current_user = User::find(auth()->user()->user_id);
-        $posts = Post::where('user_id',$current_user->user_id)->paginate(3);
+        $posts = Post::where('user_id', $current_user->user_id)->paginate(3);
         $title = "My posts";
-        if($request->ajax()){
-            return view('post.post_data', compact('posts','title'))->render();
+        if ($request->ajax()) {
+            return view('post.post_data', compact('posts', 'title'))->render();
         }
-        return view('post.posts', compact('posts','title'));
+        return view('post.posts', compact('posts', 'title'));
     }
 
     // public function unactive_post(Request $request){
--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -69,6 +69,7 @@ class PostSeeder extends Seeder
             'title' => "Laravel",
             'content' => "
                     ## About Laravel
+                    ![](https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg)
 
                     Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in many web projects, such as:
 
